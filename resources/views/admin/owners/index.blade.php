8613c1923c3b9205fbb60690a5797ae7548b03f9
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-11-02 20:18:46 +0900

    登録

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -11,7 +11,9 @@
         <div class="p-6 bg-white border-b border-gray-200">
           <section class="text-gray-600 body-font">
             <div class="container px-5 mx-auto">
-
+              <div class="flex justify-end mb-4">
+                <button onclick="location.href='{{ route('admin.owners.create')}}'" class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+              </div>
               <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                 <table class="table-auto w-full text-left whitespace-no-wrap">
                   <thead>
@@ -35,14 +37,6 @@
                     @endforeach
                   </tbody>
                 </table>
-              </div>
-              <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
-                <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-                <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
               </div>
             </div>
           </section>
